4b2dab5c8d048b469b3d94a4d949f020717cc053
ADJ0609 <adj0803@gmail.com>
2022-07-07 05:17:31 -0400

    finalized

--- scripts/index.js
+++ scripts/index.js
@@ -30,16 +30,16 @@ class Question {
     this.indexOfCorrectChoice = indexOfCorrectChoice;
   }
 }
-const QUESTION_1 = new Question("Commonly used data types DO NOT include: ", 
-  ["Strings", "Booleans", "Alerts", "Numbers"], 2);
-const QUESTION_2 = new Question("The condition in an if / else statement is enclosed within ____.", 
-  ["Quotes", "Curly brackets", "Parentheses", "Square brackets"], 2);
-const QUESTION_3 = new Question("Arrays in JavaScript can be used to store ____.", 
-  ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"], 3);
-const QUESTION_4 = new Question("String values must be enclosed within _____ when being assigned to variables.", 
-  ["Commas", "Curly brackets", "Quotes", "Parentheses"], 2);
-const QUESTION_5 = new Question("A very useful tool used during development and debugging for printing content to the debugger is: ", 
-  ["JavaScript", "Terminal/Bash", "For Loops", "console.log"], 3);
+const QUESTION_1 = new Question("Which Superhero is made out of an uncommon steel: ", 
+  ["Superman", "Wolverine", "Magneto", "antman"], 2);
+const QUESTION_2 = new Question("Which XMen discovered their power in a concentration camp?", 
+  ["Captain America", "Magneto", "Storm", "Cyclopse"], 2);
+const QUESTION_3 = new Question("Where does Harry and Harmonie watch snape die?", 
+  ["In the courtyard", "quiddich arena", "boathouse", "All of the above"], 3);
+const QUESTION_4 = new Question("May the odds forever be _____", 
+  ["lost", "in your favor", "Questionable", "in the hand of the beholder"], 2);
+const QUESTION_5 = new Question("Love is easy, forgetting is hard. Who wrote this poem: ", 
+  ["Shakespear", "Jack Nickleson", "Pablo Neruda", "Edgar Allen Poe"], 3);
 const QUESTION_LIST = [QUESTION_1, QUESTION_2, QUESTION_3, QUESTION_4, QUESTION_5];
 
 let currentQuestion = 0;
